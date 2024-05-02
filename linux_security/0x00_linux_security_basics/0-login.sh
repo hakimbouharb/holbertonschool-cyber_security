6200ba44f347a81c93cb0ec7dfc3ad85273c7fb8
Doctor Wise <135837901+hakimbouharb@users.noreply.github.com>
2024-05-02 14:11:35 +0100

    Update 0-login.sh

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-if [[ $(id -u) -ne 0 ]]; then
-last -n 5
-echo "wtmp begins $(last -F | grep -m 1 -o '^[a-zA-Z0-9]\+')"
+[[ $EUID -ne 0 ]] && >&2; exit1; sudo last -n 5 | awk '{print $1,$5,$6,$7,$8,$9}'
