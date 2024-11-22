b283da27534bd67c72d74f3258a31fc19832c0c3
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 <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>
2024-11-22 07:13:39 -0500

    fixing error

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# This script requires root or sudo privileges to show login info
-
-# Check if the script is run with root privileges
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root or with sudo."
-  exit 1
-fi
-
-# Show the last 5 login sessions with user names and their corresponding dates
-last -n 5
+[ $EUID -ne 0 ] && echo "Run as root or sudoer." && exit 1; last -n 5
