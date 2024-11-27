9b6fa49dfef088389a87c99bda5b55ef8722e6e9
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 <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>
2024-11-27 14:21:46 -0500

    whous error

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F ': ' '/^(Registrant|Admin|Tech)/ { printf("%s,%s\n", $1, $2) }'
+sudo whois "$1" | awk 'BEGIN {FS=": ";OFS=", "} /(Registrant|Admin|Tech)/ {print $1,$2}' > "$1".csv
