2153eaaa92bdbaedabde134038ef05a5f337ba65
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 <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>
2024-12-18 08:24:25 -0500

    error 2

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-hash=$1
+password="$1"
+password="${password#'{xor}'}"
+decoded_password=$(echo "$password" | base64 -d)
 
-hash=${hash#'{xor}'}
-
-decoded_pass=$(echo "$hash" | base64 -d)
-
-decodedp_xor=""
-for ((i = 0; i < ${#decoded_pass}; i++)); do
-    char="${decoded_pass:$i:1}"
+decoded_password_xor=""
+for ((i = 0; i < ${#decoded_password}; i++)); do
+    char="${decoded_password:$i:1}"
     ascii_value=$(printf "%d" "'$char")
-    xor_result=$((ascii_value ^ 95))
-    decodedp_xor+=$(printf '\\x%x' $xor_result)
+    xor_result=$(( ascii_value ^ 95 ))
+    decoded_password_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
 done
-
-echo -e "$decodedp_xor"
+echo "$decoded_password_xor"
