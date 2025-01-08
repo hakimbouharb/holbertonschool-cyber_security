f931126e8492489982b1aabe0e8b37216651dabc
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 <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>
2025-01-08 12:49:43 -0500

    fixing error

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | tail -n +2 | head -n -1 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"
